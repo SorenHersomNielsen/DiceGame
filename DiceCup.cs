3ae112a43f1d2a9c35aa463815c7e9835cf289ca
SorenHersomNielsen <hersom@ellekrattet.dk>
2020-03-30 13:43:24 +0200

    Initial commit

---
+++ DiceCup.cs
@@ -0,0 +1,37 @@
+﻿namespace DiceGame
+{
+    /// <summary>
+    /// This class represents a dice cup containing two dice.
+    /// </summary>
+    public class DiceCup
+    {
+        #region Instance fields
+        private Die _die1;
+        private Die _die2;
+        private Die _die3;
+        private int TotalValue;
+        #endregion
+
+        #region Constructor
+        public DiceCup()
+        {
+            _die1 = new Die();
+            _die2 = new Die();
+            _die3 = new Die();
+        }
+        #endregion
+
+        
+
+        // Implement a property TotalValue: the sum of 
+        // the face values of the dice in the cup
+        // Implement a method Shake: all the dice in the cup should be rolled
+        public void Shake()
+        {
+        _die1.Roll();
+        _die2.Roll();
+        _die3.Roll();
+        TotalValue = _die1.FaceValue + _die2.FaceValue + _die3.FaceValue;
+        }
+        }   
+         }
